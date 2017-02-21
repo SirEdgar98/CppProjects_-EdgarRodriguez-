8e2bfe06a8a6b2176362984fbd15f9ed859aab3b
Edgar Rodriguez <edgarrodriguezdelaca@S3-05>
2017-02-21 10:00:24 +0100

    Operador sobrecargado y Inicio random enemy

--- RandomEnemiGenerator/RandomEnemieGenerator.cpp
+++ RandomEnemiGenerator/RandomEnemieGenerator.cpp
@@ -14,27 +14,36 @@ struct Enemy
 	{
 		switch (Type)
 		{
-		case EnemyType::ZOMBIE: return "Zombie";
-		case EnemyType::VAMPIRE: return "Vampire";
-		case EnemyType::WITCH: return "Witch";
-		case EnemyType::GHOST: return "Ghost";
+		case EnemyType::ZOMBIE:		return "Zombie"; // el uso del :: para evitar ambiguedades, decalara a que pertenece el ENUM
+		case EnemyType::VAMPIRE:	return "Vampire";
+		case EnemyType::WITCH:		return "Witch";
+		case EnemyType::GHOST:		return "Ghost";
 		default: return "";
 		}
 	}
 };
-bool operator == (Enemy Malo1, Enemy Malo2)
+
+bool operator == (const Enemy &Malo1, const Enemy &Malo2) // Sobrecarga delm operador == para comprobar el tipo y nombre de dos enemigos
+											  // Se puede poner el conts, el uso del & para estar seguros de que lo pasa por referencia
 {
-	bool IDEM = false;
+	// MI FORMA (MAS LARGA, QUIZAS MENOS EFICIENTE )
+	/*bool IDEM = false;
 
 	if ((Malo1.Type == Malo2.Type) && (Malo1.name == Malo2.name))
 	{
 		IDEM = true;
 		return IDEM;
-	}
+	}*/
+
+	return Malo1.name == Malo2.name && Malo1.Type == Malo2.Type;
+	
 }
 
-Enemy CreateRandomEnemy()
+Enemy CreateRandomEnemy() // Retorna un enemigo random
 {
+	static const int MAX_LIFE{ 500 }; // Maxima vida que tendran los enemigos
+	static const std::string NAMES[]{"PACO","ELDRICH", "JACINO", "ABICHUELO", "VISUALSTUDIO", "THNDERCLAW"}; // Lista de los posibles nombres de los enemigos
+
 
 }
 void main()
