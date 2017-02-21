f4390fea18d517da7063f378e45688f6292ced4d
Edgar Rodriguez <edgarrodriguezdelaca@S3-05>
2017-02-21 09:45:43 +0100

    Switch Enemy dentro del struct

--- RandomEnemiGenerator/RandomEnemieGenerator.cpp
+++ RandomEnemiGenerator/RandomEnemieGenerator.cpp
@@ -2,13 +2,25 @@
 #include<string>
 #include<ctime>
 
-enum class EnemyType { ZOMBIE, VAMPIRE,GHOST,WITCH};
+enum class EnemyType { ZOMBIE, VAMPIRE,GHOST,WITCH,MAX }; // MAX para saber el ultimo a la hora de hacer un random
 
 struct Enemy 
 {
 	EnemyType Type;
 	std:: string name;
 	int Health;
+
+	std::string getEnemyTypeString() //FUNCION PARA CONVERTIR EL TIPO A SU NOMBRE
+	{
+		switch (Type)
+		{
+		case EnemyType::ZOMBIE: return "Zombie";
+		case EnemyType::VAMPIRE: return "Vampire";
+		case EnemyType::WITCH: return "Witch";
+		case EnemyType::GHOST: return "Ghost";
+		default: return "";
+		}
+	}
 };
 bool operator == (Enemy Malo1, Enemy Malo2)
 {
