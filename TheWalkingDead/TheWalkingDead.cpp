a7e3013ce3798308b71b6851d0077a04e37deb7a
Edgar Rodriguez <edgarrodriguezdelaca@S3-05>
2017-02-23 13:43:31 +0100

    Final de los metodos y inicio del main

--- TheWalkingDead/TheWalkingDead.cpp
+++ TheWalkingDead/TheWalkingDead.cpp
@@ -10,13 +10,12 @@ class Zombie;
 //CLASE DE PLAYER
 class Player
 {
-private:
+public:
 	//Atributos
 	Weapon weapon; // de FISITS A MACHINE_GUN
 	float precition;   // de 0.f a 1.f
 	int life;		   // de 0 a 100
 
-public:
 	Player();    //constructor
 	~Player();	 // Destructor
 	//Metodos
@@ -24,35 +23,42 @@ public:
 	bool isAlive();
 };
 //CONSTRUCTOR DEL PLAYER
-Player::Player():weapon(static_cast<Weapon>(rand()%static_cast<int>(Weapon::MAX))),precition((rand()% 11) / 10.0),life(rand() % 101)
+Player::Player():weapon(static_cast<Weapon>(rand()%static_cast<int>(Weapon::MAX))),precition((rand()% 10) / 10.0),life(rand() % 101)
 {
 }
 //ATTAQUE DEL PLYER
 void Player::attack(Zombie  &ZOMB) 
 {
-	
+		ZOMB.life -= (static_cast<int>(weapon))*precition;
 }
 //COMPROBADOR DE VIDA DEL PLAYER
 bool Player::isAlive()
 {
-
+	if (life <= 0)
+	{
+		return false;
+	}
+	else 
+	{
+		return true;
+	}
 }
 //DESTRUCTOR DEL PLAYER
 Player::~Player()
 {
 }
 
+
 //CLASE ZOMBIE
 class Zombie
 {
-private:
+public:
 	//Atributos
 	int distanceToPlayer; // de 20 a 200
 	float speed;          // de 0.f a 20.f
 	float damange;		  // de 0.f a 20.f
 	int life;			  // de 0 a 100
 
-public:
 	Zombie();  //Constructor
 	~Zombie(); // Destructor
 	//Metodos
@@ -61,18 +67,32 @@ public:
 
 };
 //CONSTRUCTOR DEL ZOMBIE
-Zombie::Zombie()
+Zombie::Zombie():distanceToPlayer(rand() % 200),speed((rand() % 21)/1.0),damange((rand() % 21) / 1.0),life(rand() % 101)
 {
 }
 //ATAQUE DEL ZOMBIE
 void Zombie::attack(Player &PJ1)
 {
-
+	if (distanceToPlayer <= 0)
+	{
+		PJ1.life -= damange;
+	}
+	else if (Zombie::distanceToPlayer > 0)
+	{
+		distanceToPlayer--;
+	}
 }
 //COMPROBADOR DE VIDA DEL ZOMBIE
 bool Zombie::isAlive()
 {
-
+	if (life <= 0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
 //DESTRUCTOR DEL ZOMBIE
 Zombie::~Zombie()
@@ -82,6 +102,16 @@ Zombie::~Zombie()
 
 void main(void)
 {
+	srand(time(nullptr));
 	Player PJ1;
-	Zombie ZOMB1;
+
+	const int NUMBER_ZOMBIES = 10;
+	Zombie Zombies[NUMBER_ZOMBIES];
+	bool ZombiesAreAlive;
+
+	do
+	{
+
+	} while (Player::isAlive || Zombie::isAlive);
+
 }
