7506f0ef856b2d581eef254aa3fb607c5aee0ef6
Edgar Rodriguez <edgarrodriguezdelaca@S3-05>
2017-02-23 13:03:04 +0100

    Metodos de Player y zombie

--- TheWalkingDead/TheWalkingDead.cpp
+++ TheWalkingDead/TheWalkingDead.cpp
@@ -5,40 +5,44 @@
 
 
 enum class Weapon { FISTS, GUN, SHOTGUN, REVOLVER,SNIPER,MACHINE_GUN, MAX };
+//FORWA
 class Zombie;
+//CLASE DE PLAYER
 class Player
 {
 private:
 	//Atributos
-	Weapon WeaponHand; // de FISITS A MACHINE_GUN
+	Weapon weapon; // de FISITS A MACHINE_GUN
 	float precition;   // de 0.f a 1.f
 	int life;		   // de 0 a 100
 
 public:
-	Player(WeaponHand,precition,life);//constructor
-	~Player();						  // Destructor
+	Player();    //constructor
+	~Player();	 // Destructor
 	//Metodos
-	void attack(Zombie &);
+	void attack(Zombie &); 
 	bool isAlive();
 };
-
-Player::Player(WeaponHand,precition,life)
+//CONSTRUCTOR DEL PLAYER
+Player::Player():weapon(static_cast<Weapon>(rand()%static_cast<int>(Weapon::MAX))),precition((rand()% 11) / 10.0),life(rand() % 101)
+{
+}
+//ATTAQUE DEL PLYER
+void Player::attack(Zombie  &ZOMB) 
 {
 	
 }
-void Player::attack(Zombie  &ZOMB1) 
-{
-
-}
+//COMPROBADOR DE VIDA DEL PLAYER
 bool Player::isAlive()
 {
 
 }
+//DESTRUCTOR DEL PLAYER
 Player::~Player()
 {
 }
 
-
+//CLASE ZOMBIE
 class Zombie
 {
 private:
@@ -49,13 +53,28 @@ private:
 	int life;			  // de 0 a 100
 
 public:
+	Zombie();  //Constructor
+	~Zombie(); // Destructor
+	//Metodos
+	void attack(Player &);
+	bool isAlive();
 
 };
-
+//CONSTRUCTOR DEL ZOMBIE
 Zombie::Zombie()
 {
 }
+//ATAQUE DEL ZOMBIE
+void Zombie::attack(Player &PJ1)
+{
 
+}
+//COMPROBADOR DE VIDA DEL ZOMBIE
+bool Zombie::isAlive()
+{
+
+}
+//DESTRUCTOR DEL ZOMBIE
 Zombie::~Zombie()
 {
 }
