5ae380cdb4a2d2838452dd98c5e3681ba012b46b
Edgar Rodriguez <edgarrodriguezdelaca@S3-05>
2017-03-09 13:58:56 +0100

    PUSH CORREGIDO

--- DynArray/DynArray.cc
+++ DynArray/DynArray.cc
@@ -4,24 +4,31 @@
 
 	// constructors
 DynArray::DynArray(void) : //POR DEFECTO; SIN ARGUMENTOS; HA DE CONSTRUIR UNA ARRAY PARA LA CONSTANTE DEFAULT { 8 }  CAPACITY = 8 // SIZE = 0
-	m_capacity{ DYN_ARRAY_DEFAULT_SIZE }, m_size{ 0 }, m_data{new int[DYN_ARRAY_DEFAULT_SIZE]}
+	m_capacity{ DYN_ARRAY_DEFAULT_SIZE },
+	m_size{ 0 }, 
+	m_data{new int[DYN_ARRAY_DEFAULT_SIZE]}
 	{};
-	
 DynArray :: DynArray(size_t size, const int &value) : // DECALRA UNA ARRAY DE N ELEMENTOS I LOS INICIALIZA EN UN VALOR
 		m_capacity{ size }, m_size{ size }, m_data{new int [size]}
 {
 	fill(begin(), end(), value);
 }; 
+DynArray::DynArray(int *arr, size_t size) :
+	m_capacity(size),
+	m_size(size),
+	m_data(new int[size])
+{
 
+};
 
-	// destructor
+// destructor
 DynArray :: ~DynArray(void) 
 {
 		delete[] m_data;
 		m_data = nullptr;
 }
 		
-	
+// asigment operators
 DynArray&  DynArray :: operator= (const DynArray &x)
 {
 	m_capacity = x.m_capacity;
@@ -37,18 +44,41 @@ DynArray&  DynArray :: operator= (const DynArray &x)
 	return *this;
 }
 
-
- int* DynArray :: begin(void) const		// DIRECION DEL PRIMER ELEMENTO DE LA ARRAY
+// iterator methods
+int* DynArray :: begin(void) const		// DIRECION DEL PRIMER ELEMENTO DE LA ARRAY
 {
 	 return m_data;
 }
-
 int* DynArray :: end(void) const		// DIRECCION DEL ULTIMO ELEMENTO DE LA ARRAY
 {
 	return m_data + m_size;
 }
 
+// modifiers methods
+void DynArray::push(const int &val)
+{
+	
+	if (m_size == m_capacity)
+	{
+		int *tmp = new int[m_capacity += m_size/2]; // RESERVA UN NUEVO ESPACIO CON LA CAPACIDAD QUE TENIA +1
+		
+		// COPIAR LOS ELEMENTOS QUE YA TENIA AL NUEVO ESPACIO GUARDADO
+		for (int i{ 0 }; i < m_size; i++)
+		{
+			tmp[i] = m_data[i];
+		}
+		//PONER EL VALOR EN LA ULTIMA POSICION
+		//tmp[m_size] = val;
+		//BORRAR EL CONTENIDO DE m_data
+		delete[] m_data;
+		// ACTUALIZAR EL PUNTERO DE M_DATA A DONDE APUNTABA TMP
+		m_data = tmp;
+	}
+	m_data[m_size] = val;
+	m_size++;
+}
 
+// relational operators
 bool operator== (const DynArray& lhs, const DynArray& rhs)	// checks if 2 arrays are equal (AL SER FRIEND; PUEDE ACCEDER A LOS ATRIBUTOS PRIVADOS DE LA ARRAY)
 {
 	if (lhs.m_size != rhs.m_size)
@@ -67,6 +97,7 @@ bool operator== (const DynArray& lhs, const DynArray& rhs)	// checks if 2 arrays
 	return true;
 }
 
+// utils methods
 void DynArray:: fill(int *first, int *last, int value) // assigns the given value to the elements in the range [first, last)
 { 
 	/*for (int *i{ first }; i < last; i++)
